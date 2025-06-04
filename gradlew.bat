1f7ce99ae9f2b896c267c099eb452171c4705d08
chowiekomba <chowiekomba@gmail.com>
2025-06-04 16:44:15 -0600

    Update gradlew.bat to 1.21.5

--- gradlew.bat
+++ gradlew.bat
@@ -35,15 +35,15 @@ set APP_HOME=%DIRNAME%
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+@rem Add default JVM options here. Increased memory for Fabric modding.
+set DEFAULT_JVM_OPTS="-Xmx2g" "-Xms512m"
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
-if %ERRORLEVEL% equ 0 goto execute
+if %ERRORLEVEL% equ 0 goto checkJavaVersion
 
 echo. 1>&2
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH. 1>&2
@@ -57,7 +57,7 @@ goto fail
 set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-if exist "%JAVA_EXE%" goto execute
+if exist "%JAVA_EXE%" goto checkJavaVersion
 
 echo. 1>&2
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
@@ -67,11 +67,21 @@ echo location of your Java installation. 1>&2
 
 goto fail
 
+:checkJavaVersion
+@rem Check if Java version is at least 21
+for /f "tokens=3" %%i in ('%JAVA_EXE% -version 2^>^&1 ^| findstr /i "version"') do set JAVA_VERSION=%%i
+set JAVA_VERSION=%JAVA_VERSION:"=%
+for /f "delims=. tokens=1" %%i in ("%JAVA_VERSION%") do set JAVA_MAJOR_VERSION=%%i
+if %JAVA_MAJOR_VERSION% LSS 21 (
+    echo. 1>&2
+    echo ERROR: Java 21 or higher is required for Minecraft 1.21.5. Found Java version %JAVA_VERSION%. 1>&2
+    echo Please update your Java installation and set JAVA_HOME accordingly. 1>&2
+    goto fail
+)
+
 :execute
 @rem Setup the command line
-
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
 
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
